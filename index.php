5b591aa48668dc484ebf86431428affe48111db0
zarszz <zarszzntunx@gmail.com>
2018-11-30 19:54:45 +0700

    Update index.php

--- index.php
+++ index.php
@@ -138,11 +138,11 @@ $app->post('/webhook', function ($request, $response) use ($bot, $pass_signature
                 if(strtolower($textmessageFromUser) == "buah"){
                     //set the carousel template
                     $carouselTemplateBuilder = new CarouselTemplateBuilder([
-                        new CarouselColumnTemplateBuilder("Apel", "text","https://i.ibb.co/MnmMzfr/apel.jpg",
+                        new CarouselColumnTemplateBuilder("Apel", "Apel sangat bermanfaat bagi tubuh, seperti sebagai antioksidan, menetralisir racun dalam butuh, kemudian karena apel kaya akan serat sehingga dapat mengurangi tinkat kolestrol,diare dan sembelit.","https://i.ibb.co/MnmMzfr/apel.jpg",
                     [
                         new UriTemplateActionBuilder('lebih lanjut',"https://cerpin.com/alasan-penting-untuk-makan-apel/"),
                     ]),
-                        new CarouselColumnTemplateBuilder("Jeruk", "text","https://i.ibb.co/g43x0ZD/jeruk.jpg",
+                        new CarouselColumnTemplateBuilder("Jeruk", "Jeruk adalah salah satu buah yang memiliki kandungan vitamin C yang tinggi, sehingga dapat menjadi sistem imun, memantau diet, mencegah kanker, hingga mencegah kerusakan kulit.","https://i.ibb.co/g43x0ZD/jeruk.jpg",
                     [
                         new UriTemplateActionBuilder('lebih lanjut',"https://manfaat.co.id/25-manfaat-buah-jeruk-untuk-kesehatan-dan-kecantikan"),
                     ]),
